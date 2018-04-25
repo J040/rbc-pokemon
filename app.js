72d6e564293fa9aec1434a444904a6127d724ab5
Rudson Mendes <rudsonm>
2018-04-25 17:33:04 -0300

    adiciona api web

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(fileUpload());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
--- controllers/rbc.controller.js
+++ controllers/rbc.controller.js
@@ -1,9 +1,19 @@
 module.exports = function(router, graph) {
     // GET
-    router.get('/batalha/:p1/:p2', );
+    router.get('/batalha/:p1/:p2', seguir);
 
     function seguir(request, response) {
         let p1 = +request.params.p1;
         let p2 = +request.params.p2;
+        let winner = Math.random() >= 0.5 ? p1 : p2;
+        response.send(`<div style='display: -webkit-flex;
+                                display: -ms-flexbox;
+                                display: flex;
+                                justify-content: center;
+                                width: 100%;
+                                position: fixed;
+                                float: left;
+                                top: 40%'>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${winner}.png"/> </div>`);
     }
 }
