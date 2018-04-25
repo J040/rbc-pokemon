27aeedb964534a763bc41416a2f6dd61f4cce6b2
Rudson Mendes <rudsonm>
2018-04-25 17:19:41 -0300

    api

---
+++ app.js
@@ -0,0 +1,23 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(fileUpload());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, User-Agent");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next();
+});
+
+const router = express.Router();
+
+require('./controllers/rbc.controller.js')(router);
+
+app.use('/api', router);
+
+app.listen(9191, function() {
+  console.log('Server online on port: 9191');
+});
---
+++ controllers/rbc.controller.js
@@ -0,0 +1,9 @@
+module.exports = function(router, graph) {
+    // GET
+    router.get('/batalha/:p1/:p2', );
+
+    function seguir(request, response) {
+        let p1 = +request.params.p1;
+        let p2 = +request.params.p2;
+    }
+}
