88fac3438fc60db0d7350cde231ad5774b2df39d
Rudson Mendes <rudsonm>
2018-04-30 13:53:46 -0300

    mudanças que eu nao me recordo.

---
+++ client/module.js
@@ -0,0 +1,7 @@
+angular
+    .module("PokeModule", [])
+    .controller("PokeController", PokeController);
+
+function PokeController($scope, $http) {
+    $scope.menssagem = "OIADIOASJDOIA";
+}
--- server/controllers/rbc.controller.js
+++ server/controllers/rbc.controller.js
@@ -6,31 +6,55 @@ module.exports = function(router) {
 
     function seguir(request, response) {
         let result = new Combat(
-            new Pokemon(39, 52, 43, 60, 50, 65, 'fire'),
-            new Pokemon(45, 49, 49, 65, 65, 45, 'grass', 'poison'),
+            POKEMONS[request.params.p1],
+            POKEMONS[request.params.p2],
             0
         );
 
+        let similarities = [];
+
         let highestSim = 0;
-        let bestCase;
         for(let i = 0; i < COMBATS.length; i++) {
             let similarity1 = 0;
-            similarity1 += result.pokemon1.getSimilarityValue(COMBATS[i].pokemon1);
-            similarity1 += result.pokemon2.getSimilarityValue(COMBATS[i].pokemon2);
+            similarity1 += result.pokemon1.getSimilarityValue(POKEMONS[COMBATS[i].pokemon1]);
+            similarity1 += result.pokemon2.getSimilarityValue(POKEMONS[COMBATS[i].pokemon2]);
             similarity1 /= 2;
 
             let similarity2 = 0;
-            similarity2 += result.pokemon1.getSimilarityValue(COMBATS[i].pokemon2);
-            similarity2 += result.pokemon2.getSimilarityValue(COMBATS[i].pokemon1);
+            similarity2 += result.pokemon1.getSimilarityValue(POKEMONS[COMBATS[i].pokemon2]);
+            similarity2 += result.pokemon2.getSimilarityValue(POKEMONS[COMBATS[i].pokemon1]);
             similarity2 /= 2;
-
-            let similarity = (similarity1 > similarity2) ? similarity1 : similarity2;
-            console.log(i, similarity);
-            if(similarity >= highestSim) {                
-                bestCase = COMBATS[i];
+                        
+            let similarity;
+            let winner;
+            if(similarity1 > similarity2) {
+                similarity = similarity1;
+                winner = (COMBATS[i].winner === COMBATS[i].pokemon1) ? result.pokemon1.dex : result.pokemon2.dex;
+            } else {
+                similarity = similarity2;
+                winner = (COMBATS[i].winner === COMBATS[i].pokemon1) ? result.pokemon2.dex : result.pokemon1.dex;
             }
+
+            if(similarity >= highestSim) {
+                highestSim = similarity;
+                if(similarity1 > similarity2)
+                    result.winner = (winner === COMBATS[i].pokemon1) ? result.pokemon1.dex : result.pokemon2.dex;
+                else
+                    result.winner = (winner === COMBATS[i].pokemon1) ? result.pokemon2.dex : result.pokemon1.dex;
+            }
+
+            similarities.push({
+                p1: POKEMONS[COMBATS[i].pokemon1], 
+                p2: POKEMONS[COMBATS[i].pokemon2], 
+                winner: COMBATS[i].winner, 
+                similarity: similarity
+            });
         }
-        result.winner = bestCase.winner;
+        console.log("VENCEDORRRR: ", result.winner);
+        similarities
+            .sort((a, b) => b.similarity - a.similarity)
+            .slice(0, 10)
+            .forEach(s => console.log('sim: ', s.similarity, ' p1: ', s.p1.dex, s.p1.name, ' p2: ', s.p2.dex, s.p2.name, ' w: ', s.winner));        
 
         response.send(`<div style='display: -webkit-flex;
                                 display: -ms-flexbox;
@@ -45,30 +69,28 @@ module.exports = function(router) {
 }
 
 /* INITIALIZING */
-const MAX_NUM_COMBATS = 30;
 let EFFECTIVENESS_TABLE;
-let COMBAT;
+let COMBATS;
+let POKEMONS;
 generateEffectivenessTable();
 getCombatsLog();
+getPokemons();
 
 /* POKEMON */
 
 class Pokemon {
-    constructor(hp, attack, defense, spAttack, spDefense, speed, type1, type2) {
-        this.hp = hp;
-        this.attack = attack;
-        this.defense = defense;
-        this.spAttack = spAttack;
-        this.spDefense = spDefense;
-        this.speed = speed;
+    constructor(dex, name, stats, type1, type2) {
+        this.dex = dex;
+        this.name = name;
+        this.stats = stats;
         type2 = type2 || type1;
         this.types = [new Type(type1), new Type(type2)];
     }
 
     getSimilarityValue(pokemon) {
         let sum = 0;
-        for(const stat of Object.keys(STAT))
-            sum += getSimilarityNumberValue(this[stat], pokemon[stat], STAT[stat].min, STAT[stat].max);
+        for(const s of Object.keys(this.stats))
+            sum += getSimilarityNumberValue(this.stats[s], pokemon.stats[s], STATS[s].min, STATS[s].max);
         
         let sim1 = 3 * getSimilarityTypeValue(this.types[0], pokemon.types[0])
                  + 3 * getSimilarityTypeValue(this.types[1], pokemon.types[1]);
@@ -77,24 +99,35 @@ class Pokemon {
                  + 3 * getSimilarityTypeValue(this.types[1], pokemon.types[0]);        
         sum += (sim1 > sim2) ? sim1 : sim2;
         
-        return sum / (Object.keys(STAT).length + 6);
+        return sum / (Object.keys(this.stats).length + 6);
     }
 }
 
-class Stat {
+class Stats {
+    constructor(hp, attack, defense, spAttack, spDefense, speed) {
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.spAttack = spAttack;
+        this.spDefense = spDefense;
+        this.speed = speed;
+    }
+}
+
+class MinMaxStat {
     constructor(min, max) {
         this.min = min;
         this.max = max;
     }
 }
 
-const STAT = {
-    hp: new Stat(1, 255),
-    attack: new Stat(5, 190),
-    defense: new Stat(5, 230),
-    spAttack: new Stat(10, 194),
-    spDefense: new Stat(20, 230),
-    speed: new Stat(5, 150)
+const STATS = {
+    hp: new MinMaxStat(1, 255),
+    attack: new MinMaxStat(5, 190),
+    defense: new MinMaxStat(5, 230),
+    spAttack: new MinMaxStat(10, 194),
+    spDefense: new MinMaxStat(20, 230),
+    speed: new MinMaxStat(5, 150)
 }
 
 function getSimilarityNumberValue(a, b, min, max) {
@@ -117,7 +150,7 @@ class Type {
     }
 }
 
-function getEffectiveness(type) {
+function getEffectiveness(type) {    
     return EFFECTIVENESS_TABLE[type.toLowerCase()];
 }
 
@@ -141,25 +174,40 @@ class Combat {
     }
 }
 
+function getPokemons(dex) {
+    POKEMONS = {};
+    fs.readFile("./data/pokemon.csv", "utf-8", (error, pokemon) => {
+        let pokemons = pokemon.split('\n');
+        for(let i = 1; i < pokemons.length; i++) {
+            let p = pokemons[i].split(',');
+            POKEMONS[p[0]] = new Pokemon(
+                                p[0], p[1], 
+                                new Stats(p[4], p[5], p[6], p[7], p[8], p[9]), 
+                                p[2], p[3]
+                            );
+        }
+    });
+}
+
 function getCombatsLog() {
     fs.readFile("./data/combats.csv", "utf-8", (error, combats) => {
         combats = combats.split('\n');        
         COMBATS = [];
         fs.readFile("./data/pokemon.csv", "utf-8", (error, pokemons) => {            
             pokemons = pokemons.split('\n');
-            for(let i = 1; i < MAX_NUM_COMBATS; i++) {
+            for(let i = 1; i < pokemons.length; i++) {
                 let pokemon1 = combats[i].split(',')[0];
                 let pokemon2 = combats[i].split(',')[1];
-                let winner = (combats[i].split(',')[2] == pokemon1) ? 1 : 2;
+                let winner = combats[i].split(',')[2];
                 let found = 0;
                 for(let j = 1; j < pokemons.length; j++) {
                     let p = pokemons[j].split(',');                    
                     if(typeof(pokemon1) != 'object' && pokemon1 == p[0]) {
-                        pokemon1 = new Pokemon(p[4], p[5], p[6], p[7], p[8], p[9], p[2], p[3]);
+                        pokemon1 = p[0];
                         found++;
                     }
                     if(typeof(pokemon2) != 'object' && pokemon2 == p[0]) {
-                        pokemon2 = new Pokemon(p[4], p[5], p[6], p[7], p[8], p[9], p[2], p[3]);
+                        pokemon2 = p[0];
                         found++;
                     }
                     if(found == 2)
