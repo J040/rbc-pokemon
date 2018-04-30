8c3649ed3256a25353d734967bf751b4b14919ec
J040 <umvelhobarbudo@gmail.com>
2018-04-30 15:11:05 -0300

    Modificações Index.html

--- client/module.js
+++ client/module.js
@@ -2,6 +2,14 @@ angular
     .module("PokeModule", [])
     .controller("PokeController", PokeController);
 
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
+
 function PokeController($scope, $http) {
-    $scope.menssagem = "OIADIOASJDOIA";
+    $scope.mensagem1 = "Pokémon 1";
+    $scope.mensagem2 = "Pokémon 2";
+    $scope.p1 = {};
+    $scope.p2 = {};
+    $scope.p1 = {};
 }
